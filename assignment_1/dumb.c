58e8dad5c4b7c80a24d0ea580fa6382f21c0afb0
neerajp99 <neerajp1999@gmail.com>
2021-04-05 23:48:25 +0530

    Add assignment 1

---
+++ assignment_1/dumb.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "dumb.h"
+  
+void dumb(int n)
+{
+    printf("n = %d\n\n\n", n);
+    int k = 10;
+
+}
---
+++ assignment_1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dumb.h"
+
+int main()
+{
+    printf("%s", "Hello World!\n\n\n" );
+    dumb(10);
+
+
+    return 0;
+}
