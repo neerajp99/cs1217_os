61c66edf7d08154baa80bac3e80579de78d9f375
neerajp99 <neerajp1999@gmail.com>
2021-04-05 23:48:42 +0530

    Add assignment 3

---
+++ assignment_3/Neeraj_Pandey_CS1217_Assignment3/question-2/date.c
@@ -0,0 +1,22 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int
+main(int argc, char *argv[])
+{
+  struct rtcdate r;
+
+  if (date(&r)) {
+    printf(2, "date failed\n");
+    exit();
+  }
+  // ##################################
+  // UPDATE
+  // ##################################
+  // your code to print the time in any format you like...
+  printf(1, "%d/%d/%d  %d:%d \n", r.day, r.month, r.year, r.hour, r.minute);
+
+  exit();
+}
+
